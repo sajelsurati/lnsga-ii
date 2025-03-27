bc606d3476c2f682c0fdce608d7704b4a1f564a5
Sajel Surati <sajelsurati@gmail.com>
2025-03-26 20:00:14 -0400

    it works!!

--- Dnsga2.py
+++ Dnsga2.py
@@ -4,12 +4,13 @@ import numpy as np
 # TODO: Add constraints
 class Dnsga_II_discrete:
 
-    def __init__(self, objective_list, constraints, min_values, max_values, population_size=100, mutation_percent=.3, 
-                 dynamic_parent_percent=.1, dynamic_type='a', zeta = .4):
+    def __init__(self, objective_list, constraints, num_vars, min_values, max_values, population_size=100, mutation_percent=.3, 
+                 dynamic_parent_percent=.1, dynamic_type='a', zeta = .4, detection_cutoff = 0, nt = 10, tau_t = 5):
         self.min_values = min_values
         self.max_values = max_values
         self.objective_list = objective_list
         self.objective_num = len(objective_list)
+        self.num_vars = num_vars
         self.population_size = population_size
         self.generation_num = 0
         self.mutation_percent = mutation_percent
@@ -17,14 +18,15 @@ class Dnsga_II_discrete:
         self.parent_percent = dynamic_parent_percent
         self.dynamic_type = dynamic_type
         self.zeta = zeta
+        self.detection_cutoff = detection_cutoff
+        self.nt = nt
+        self.tau_t = tau_t
 
     def fulfills_constraints(self, population, t):
-        print(population)
         constraint_values = np.full((len(population),), True)
         for constraint in self.constraints:
             fulfills = np.apply_along_axis(constraint, 1, population, t)
             constraint_values = np.logical_and(constraint_values, fulfills)
-        print(constraint_values)
         return constraint_values
 
     def initialize_solutions_discrete(self, recalculate=False):
@@ -36,10 +38,13 @@ class Dnsga_II_discrete:
                 fulfills_constraints = np.full((replacement_size,), False)
                 new_solutions = np.empty((replacement_size, self.objective_num))
                 while (np.any(np.logical_not(fulfills_constraints))):
-                    for i in range(self.objective_num):
+                    for i in range(self.num_vars):
                         #initialize solutions to different values in the given ranges
-                        solution_values = np.random.randint(self.min_values[i], self.max_values[i], 
+                        solution_values = np.random.uniform(self.min_values[i], self.max_values[i], 
                                                             (len(np.where(np.logical_not(fulfills_constraints))[0]),))
+                        #below is just for integers
+                        # solution_values = np.random.randint(self.min_values[i], self.max_values[i], 
+                        #                                     (len(np.where(np.logical_not(fulfills_constraints))[0]),))
                         new_solutions[np.where(np.logical_not(fulfills_constraints)),i] = solution_values
                     #checks whether constraints are fulfilled
                     fulfills_constraints = self.fulfills_constraints(solution_values, 0)
@@ -48,19 +53,21 @@ class Dnsga_II_discrete:
             self.population[replacement_arr] = new_solutions
             return
 
-        self.population = np.empty((self.population_size, self.objective_num))
+        self.population = np.empty((self.population_size, self.num_vars))
         fulfills_constraints = np.full((self.population_size,), False)
         #while the constraints haven't been fulfilled yet
         while (np.any(np.logical_not(fulfills_constraints))):
-            for i in range(self.objective_num):
+            for i in range(self.num_vars):
                 #initialize solutions to different values in the given ranges
-                solution_values = np.random.randint(self.min_values[i], self.max_values[i], 
-                                                    (len(np.where(np.logical_not(fulfills_constraints))[0]),))
+
+                solution_values = np.random.uniform(self.min_values[i], self.max_values[i], 
+                                                            (len(np.where(np.logical_not(fulfills_constraints))[0]),))
+                
+                # solution_values = np.random.randint(self.min_values[i], self.max_values[i], 
+                #                                     (len(np.where(np.logical_not(fulfills_constraints))[0]),))
                 self.population[np.where(np.logical_not(fulfills_constraints)),i] = solution_values
             #checks whether constraints are fulfilled
             fulfills_constraints = self.fulfills_constraints(self.population, 0)
-        print('population\n',self.population)
-
 
     def evaluate_fitness(self, t, population=None,):
         if population is None:
@@ -73,7 +80,7 @@ class Dnsga_II_discrete:
         for i in range(len( self.objective_list)):
             for j in range(len(population)):
                 objective_values[j,i] = self.objective_list[i](population[j], t)
-        print('values\n',objective_values)
+        # print('values\n',objective_values)
         return objective_values
 
     def non_dominated_sorting(self, values, population=None):
@@ -88,12 +95,12 @@ class Dnsga_II_discrete:
         #an nxn matrix that counts how many values each solution dominates
         #column represents what rows are dominated
         dominance_list = np.zeros((len(population), len(population)))
-        print(dominance_list)
+        # print(dominance_list)
         #for each objective value, check if it dominates other values
         for i in range(len(population)):
             # all values must be geq and there must exist a strictly greater than value
-            dominance_boolean = (np.all(values[i] >= values, axis = 1) & 
-                np.any(values[i] > values, axis = 1))
+            dominance_boolean = (np.all(values[i] <= values, axis = 1) & 
+                np.any(values[i] < values, axis = 1))
             dominance_list[:,i] += dominance_boolean.astype(int)
         front_count = 0
         #until all solutions have assigned fronts
@@ -111,7 +118,7 @@ class Dnsga_II_discrete:
             trues = np.full((self.population_size,), True)
             dominance_list[np.ix_(trues, solutions_to_add)] = 0
             front_count+=1
-            print(dominance_list)
+            # print(dominance_list)
         return front_assignments
             
     def calculate_crowding_distance(self, values, population=None):
@@ -131,12 +138,15 @@ class Dnsga_II_discrete:
         return(crowding_distance)
 
     def mutation(self, child, t):
-        mutate = np.random.random((self.objective_num,))
+        # print('in mutation')
+        mutate = np.random.random((self.num_vars,))
         fulfills = np.full((self.population_size,), False) # working on this for now.
-        for i in range(self.objective_num):
+        for i in range(self.num_vars):
             if mutate[i] < self.mutation_percent:
+                # print('mutate!')
                 fulfills = self.fulfills_constraints([child], t)
-                value = np.random.randint(self.min_values[i], self.max_values[i])
+                value = np.random.uniform(self.min_values[i], self.max_values[i])
+                # value = np.random.randint(self.min_values[i], self.max_values[i])
                 child[i] = value
         return child
     
@@ -144,9 +154,9 @@ class Dnsga_II_discrete:
     def crossover(self, parent_1, parent_2):
         #select a random crossover point
         cross_over_point =  np.random.randint(0, self.objective_num)
-        print(cross_over_point)
-        child = np.zeros((self.objective_num,))
-        print(child)
+        # print(cross_over_point)
+        child = np.zeros((self.num_vars,))
+        # print(child)
         #choose objective values up until crossover point from parent 1
         child[:cross_over_point] = parent_1[:cross_over_point]
         #choose objective values after crossover point from parent 2
@@ -159,7 +169,7 @@ class Dnsga_II_discrete:
         binary_2 = np.random.randint(0,self.population_size, (2*self.population_size,))
 
         #array for remaining parents from binary tournament
-        parents = np.zeros((2*self.population_size, self.objective_num))
+        parents = np.zeros((2*self.population_size, self.num_vars))
 
         #where front of 1st solution is better than the 2nd
         binary_1_smaller = fronts[binary_1] < fronts[binary_2]
@@ -184,38 +194,44 @@ class Dnsga_II_discrete:
     def generate_children(self, fronts, crowding, t):
         #binary tournament phase:
         parents_1, parents_2 = self.binary_tournament(fronts, crowding)
-
-        children = np.empty((len(self.population), self.objective_num))
+        children = np.empty((len(self.population), self.num_vars))
 
         for i in range(len(parents_1)):
+            # print('crossover and mutation')
+
             child = self.mutation(self.crossover(parents_1[i,:], parents_2[i,:]), t)
+            while (np.any(np.all(self.population == child, axis=1))):
+                child = self.mutation(self.crossover(parents_1[i,:], parents_2[i,:]), t)
             children[i,:] = child
         return children
 
 
     def generate_new_population(self, combined_pop, t):
         objective_values = self.evaluate_fitness(t, combined_pop)
-        print(objective_values)
+        # print(objective_values)
         fronts = self.non_dominated_sorting(objective_values, combined_pop)
         crowding_values = self.calculate_crowding_distance(objective_values, combined_pop)
-        print('fronts:\n', fronts, 'crowding:\n', crowding_values)
-        print('combined population\n', objective_values)
+        # print('fronts:\n', fronts, 'crowding:\n', crowding_values)
+        # print('combined population\n', objective_values)
         sort_order = np.lexsort((-crowding_values, fronts))
         combined_pop = combined_pop[sort_order]
-        print(combined_pop)
-        print('fronts\n',fronts[sort_order])
-        print('crowding\n',crowding_values[sort_order])
+        # print(combined_pop)
+        # print('fronts\n',fronts[sort_order])
+        # print('crowding\n',crowding_values[sort_order])
         self.population = combined_pop[:self.population_size]
 
-    def detect_change(self, t):
+    def detect_change(self, gen):
+        t = 1/self.nt*(gen//self.tau_t)
         # choose random solutions to have values tested
         detection_size = min(1, int(self.population_size*self.parent_percent))
         detection_arr = self.population[np.random.choice(np.arange(self.population_size), (detection_size,)),:]
-        print('detect',detection_arr)
+        # print('detect',detection_arr)
         # if past objective values != current, return true
         objective_values_t = self.evaluate_fitness(t=t, population=detection_arr)
-        objective_values_past_t = self.evaluate_fitness(t=t-1, population=detection_arr)
-        if not np.array_equal(objective_values_t, objective_values_past_t):
+        objective_values_past_t = self.evaluate_fitness(t = 1/self.nt*((gen-1)//self.tau_t), population=detection_arr)
+        diff = (objective_values_t - objective_values_past_t)**2
+        print('detection mean:',np.abs(diff/objective_values_past_t).mean())
+        if np.abs(diff/objective_values_past_t).mean() > self.detection_cutoff:
             return True
         # if past constraint satisfaction != current, return true
         constraints_t = self.fulfills_constraints(t=t, population=detection_arr)
@@ -226,16 +242,26 @@ class Dnsga_II_discrete:
 
     def dnsga_ii_discrete(self, num_iterations=100):
         self.initialize_solutions_discrete()
-        for t in range(num_iterations):
-            if t > 0:
-                if self.detect_change(t):
-                    self.initialize_solutions_discrete(self.population)
+        values = []
+        for gen in range(num_iterations):
+            t = 1/self.nt * np.floor(gen/self.tau_t)
+            if gen > 0:
+                print(gen, t)
+                # if (t != 1/self.nt * np.floor((gen-1)/self.tau_t)):
+                #     values.append(self.evaluate_fitness(1/self.nt * np.floor((gen-1)/self.tau_t)))
+                if self.detect_change(gen):
+                    print('detecting!')
+                    self.initialize_solutions_discrete(recalculate=True)
+                    values.append(self.evaluate_fitness(1/self.nt * np.floor((gen-1)/self.tau_t)))
             objective_values = self.evaluate_fitness(t)
             fronts = self.non_dominated_sorting(objective_values)
             crowding_values = self.calculate_crowding_distance(objective_values)
+            print('children')
             children = self.generate_children(fronts, crowding_values, t)
             combined_population = np.concatenate((children, self.population))
             self.generate_new_population(combined_population, t)
-        print('final pop\n',self.population)
+        # print('final pop\n',self.population)
         objective_values = self.evaluate_fitness(t=num_iterations)
-        return self.population 
+        values.append(objective_values)
+        # return self.population
+        return values
--- run_dnsga-ii.py
+++ run_dnsga-ii.py
@@ -1,5 +1,6 @@
 from Dnsga2 import Dnsga_II_discrete
 import matplotlib.pyplot as plt
+import numpy as np
 
 def objective1(x, t):
     return 2*x[0]
@@ -10,9 +11,45 @@ def objective2(x, t):
 def equals_constraint(x, t):
     return (x[0] + x[1] == 0)
 
-dnsga = Dnsga_II_discrete(objective_list=[objective1, objective2], constraints=[equals_constraint], 
-                 min_values=[-1, -10], max_values=[9, 6], population_size=10, mutation_percent= .4)
-values = dnsga.dnsga_ii_discrete(num_iterations=2)
-print(values)
-plt.scatter(values[:,0], values[:,1])
+
+
+def zdt1_f1(x, t):
+    return x[0]
+
+def zdt_f2(x, t):
+    g = 1 + (9/(len(x)-1))*(np.sum(x)-x[0])
+    h = 1-np.sqrt(x[0]/g)
+    return (g*h)
+
+
+def df1_f1(x,t):
+    return x[0]
+
+def df1_f2(x,t):
+    print("t=",t)
+    G = np.abs(np.sin(0.5*np.pi*t))
+    H = .75*np.sin(0.5*np.pi*t) + 1.25
+    g = 1 + np.sum(np.power((np.array(x)[1:] - G),2))
+    return g*(1-np.power((x[0]/g), H))
+
+# def df1_g(x,t):
+
+
+
+# dnsga = Dnsga_II_discrete(objective_list=[objective1, objective2], constraints=[], 
+#                  num_vars=2, min_values=[-1, -10], max_values=[9, 6], population_size=100, mutation_percent= .4)
+
+
+# # FOR ZDT1
+# dnsga = Dnsga_II_discrete(objective_list=[zdt1_f1, zdt_f2], constraints=[], 
+#                  num_vars=2, min_values=([0]*30), max_values=([1]*30), population_size=100, mutation_percent= .4)
+
+dnsga = Dnsga_II_discrete(objective_list=[df1_f1, df1_f2], constraints=[], 
+                 num_vars=2, min_values=([0]*2), max_values=([1]*2), population_size=100, mutation_percent= .4, tau_t=10)
+
+solutions = dnsga.dnsga_ii_discrete(num_iterations=500)
+# print(solutions)
+for sol in solutions:
+    plt.scatter(sol[:,0], sol[:,1])
+plt.ylim((0,1))
 plt.show()
